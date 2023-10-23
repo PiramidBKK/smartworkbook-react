aaa2c5479d117353b7c38ebcc3b2cbdffb9a13fd
ratchapon-p <ratchapon-p@rmutp.ac.th>
2023-10-23 19:57:27 +0700

    data-details

--- src/components/AddNew/SwInterface/SwitchInterface.js
+++ src/components/AddNew/SwInterface/SwitchInterface.js
@@ -24,26 +24,63 @@ export default function SwinterfacePopup(){
     const swinterfaceData = swdetailName ? swdetailName.swinterfaces : [];
 
     //switch images
-    const switchImage = swdetailName?.modelimg;
 
 
 
     return (
       <div className="swinterface-popup-main">
-        <h2>Switch Interface : {swdetailName ? swdetailName.hostname : null}</h2>
-        <div className='image-of-switch'>
-          {swdetailName ? <img src={swdetailName.modelimg } alt='Switch Image' className='switch-image'></img>: null}
+        <h2>
+          Switch Interface : {swdetailName ? swdetailName.hostname : null}
+        </h2>
+        <div className="image-of-switch">
+          {swdetailName ? (
+            <img
+              src={swdetailName.modelimg}
+              alt="Switch Image"
+              className="switch-image"
+            ></img>
+          ) : null}
         </div>
         <div className="switchdetail-data">
           <div className="Hardware-detail">
-            <table>
-
+            <table className="hardware">
+              <thead>Hardware Detail</thead>
+              <tbody>
+                <tr>
+                  <td>Product ID</td>
+                  <td>{swdetailName?.model}</td>
+                </tr>
+                <tr>
+                  <td>S/N</td>
+                  <td>{swdetailName?.serialnumber}</td>
+                </tr>
+                <tr>
+                  <td>MAC ADDRESS</td>
+                  <td>{swdetailName?.macaddress}</td>
+                </tr>
+              </tbody>
             </table>
           </div>
 
           <div className="Configuration-detail">
-            <table>
-
+            <table className="configuration">
+              <thead className='sub-thead'>
+                Configuration Detail
+              </thead>
+              <tbody>
+                <tr>
+                  <td>IP Address</td>
+                  <td>{swdetailName?.ipaddress}</td>
+                </tr>
+                <tr>
+                  <td>Hostname</td>
+                  <td>{swdetailName?.hostname}</td>
+                </tr>
+                <tr>
+                  <td>Location</td>
+                  <td>{swdetailName?.location}</td>
+                </tr>
+              </tbody>
             </table>
           </div>
         </div>
@@ -59,34 +96,36 @@ export default function SwinterfacePopup(){
             <th>Ramark</th>
           </thead>
           <tbody>
-            {swinterfaceData?.map((swinterface) =>(
-                <tr key={swinterface._id}>
-                    <td>{swinterface.port}</td>
-                    <td>{swinterface.connectto}</td>
-                    <td>{swinterface.description}</td>
-                    <td>{swinterface.vlanid}</td>
-                    <td>{swinterface.mode}</td>
-                    <td>{swinterface.label}</td>
-                    <td>{swinterface.remark}</td>
-                </tr>
+            {swinterfaceData?.map((swinterface) => (
+              <tr key={swinterface._id}>
+                <td>{swinterface.port}</td>
+                <td>{swinterface.connectto}</td>
+                <td>{swinterface.description}</td>
+                <td>{swinterface.vlanid}</td>
+                <td>{swinterface.mode}</td>
+                <td>{swinterface.label}</td>
+                <td>{swinterface.remark}</td>
+              </tr>
             ))}
           </tbody>
         </table>
-    
-        <div className='popup-dvdesign-button'>
-        <Link to={`/wbdetail/${id}`} className="back-btn-dvdesign">
-          <div className="back-dvdesign">
-            <h3>Back</h3>
-          </div>
-        </Link>
 
-        <Link to={`/swinterface/${id}/${switchId}`} className="ok-btn-dvdesign">
-          <div className="ok-dvdesign">
-            <h3>OK</h3>
-          </div>
-        </Link>
+        <div className="popup-dvdesign-button">
+          <Link to={`/wbdetail/${id}`} className="back-btn-dvdesign">
+            <div className="back-dvdesign">
+              <h3>Back</h3>
+            </div>
+          </Link>
+
+          <Link
+            to={`/swinterface/${id}/${switchId}`}
+            className="ok-btn-dvdesign"
+          >
+            <div className="ok-dvdesign">
+              <h3>OK</h3>
+            </div>
+          </Link>
         </div>
-
       </div>
     );
 
