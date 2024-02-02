e5f30542da3794e5c64a1543418886acca910923
ratchapon-p <ratchapon-p@rmutp.ac.th>
2024-02-02 22:08:52 +0700

    update-baseUrl

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
       <BrowserRouter>
         {/* headerpath */}
           <HomePageHeader />
+          <Modal />
 
         <Routes>
           {/* bodypath */}
--- src/utils/reactPlayGround.js
+++ src/utils/reactPlayGround.js
@@ -1,19 +1,8 @@
-import Popup from "reactjs-popup";
 
 export default function Modal(){
     return(
-      <Popup 
-      trigger={<button>Open</button>}
-      modal
-      nested
-      >
-        {close =>{
-          <button onClick={close}>&times;</button>
-        }}
-        <div className="hello">
-          Hello
-        </div>
-      </Popup>
+
+      "hello"
 
     )
 }
